d3e1641251408942674c35bededffb4f14b746c9
Jen Lim <cmdlhz@gmail.com>
2019-11-29 15:31:27 +0900

    Spring 3.0.1으로 update

---
+++ src/main/java/com/sp/bdi/test/quiz_191129/Test7_9.java
@@ -0,0 +1,10 @@
+package com.sp.bdi.test.quiz_191129;
+
+import java.util.Scanner;
+
+public class Test7_9 {
+	Scanner scan = new Scanner(System.in);
+	int dan = scan.nextInt();
+	String danStr = "";
+	
+}
